52b1d1391f38b1ebd4b878a58c8246d00695107b
pushpak90 <pushpakafasate90@gmail.com>
2026-01-07 08:43:29 +0530

    chore: initialize Spring Boot project with Gradle

---
+++ libracore/libracore/src/main/java/com/libracore/app/LibracoreApplication.java
@@ -0,0 +1,13 @@
+package com.libracore.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibracoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibracoreApplication.class, args);
+	}
+
+}
---
+++ libracore/libracore/src/test/java/com/libracore/app/LibracoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libracore.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibracoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
