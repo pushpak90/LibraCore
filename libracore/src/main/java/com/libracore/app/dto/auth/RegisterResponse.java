e09a0c9b84edba63d1f951cc4e950cd9d9b9ddb7
pushpak90 <pushpakafasate90@gmail.com>
2026-01-08 09:19:27 +0530

    feat: add register request and response DTOs

---
+++ libracore/src/main/java/com/libracore/app/dto/auth/RegisterResponse.java
@@ -0,0 +1,10 @@
+package com.libracore.app.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RegisterResponse {
+    private String message;
+}
