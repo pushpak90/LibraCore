41817edaa0b154a46c7392f62f8392b3c40cea36
pushpak90 <pushpakafasate90@gmail.com>
2026-01-08 08:58:00 +0530

    feat: add user entity with role support

---
+++ libracore/src/main/java/com/libracore/app/entity/User.java
@@ -0,0 +1,25 @@
+package com.libracore.app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
