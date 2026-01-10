bf82415cb6c6973cd6cc0b3781ce7c2b2abc7c32
pushpak90 <pushpakafasate90@gmail.com>
2026-01-10 20:06:53 +0530

    feat: add user registration API endpoint

---
+++ libracore/src/main/java/com/libracore/app/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.libracore.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.libracore.app.dto.auth.RegisterRequest;
+import com.libracore.app.dto.auth.RegisterResponse;
+import com.libracore.app.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> Registration(@Valid @RequestBody RegisterRequest request) {
+        userService.register(request);
+        return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new RegisterResponse("User Registered successfully!"));
+    }
+    
+}
