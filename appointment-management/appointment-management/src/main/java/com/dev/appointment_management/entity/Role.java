c984dca260aa1485ecb40b64888714bb948dea6e
pushpak90 <pushpakafasate90@gmail.com>
2026-01-22 23:05:40 +0530

    Configure database and add User and Role entities

---
+++ appointment-management/appointment-management/src/main/java/com/dev/appointment_management/entity/Role.java
@@ -0,0 +1,19 @@
+package com.dev.appointment_management.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
---
+++ appointment-management/appointment-management/src/main/java/com/dev/appointment_management/entity/User.java
@@ -0,0 +1,28 @@
+package com.dev.appointment_management.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String fullName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+    @Column(nullable = false)
+    private boolean active = true;
+}
