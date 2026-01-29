f88df804116ac37a07821719b7a38dc7da171f20
pushpak90 <pushpakafasate90@gmail.com>
2026-01-29 07:47:03 +0530

    Add JWT utility for token generation and validation

---
+++ appointment-management/appointment-management/src/main/java/com/dev/appointment_management/sercurity/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.dev.appointment_management.sercurity;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final long EXPIRATION_TIME = 24 * 60 * 60 * 1000; // 1 day
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String email, String role) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims validateToken(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
